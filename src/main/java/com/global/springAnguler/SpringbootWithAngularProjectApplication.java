6072f1e41a50d56700cac811c5497c9b5745702b
Ayman <a.m_ayman9@outlook.com>
2023-04-25 14:28:29 +0200

    Creating Project Structure

---
+++ src/main/java/com/global/springAnguler/SpringbootWithAngularProjectApplication.java
@@ -0,0 +1,13 @@
+package com.global.springAnguler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithAngularProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithAngularProjectApplication.class, args);
+	}
+
+}
